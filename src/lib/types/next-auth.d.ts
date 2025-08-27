4d434e26babfb5e1d291d6610d90fa6f3461bfe2
Camilo Martinez <juancamilomabe@gmail.com>
2025-08-27 11:00:15 -0400

    refactor: move types to src/types directory for better organization

--- src/lib/types/next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth, { DefaultSession } from "next-auth"
-
-declare module "next-auth" {
-    interface Session {
-        accessToken?: string
-        error?: string
-        refreshToken?: string
-    }
-}
